401950f249f866a42fd333615e1072dc18ebb8ba
DjangoBv <renato.caccha.7@gmail.com>
2021-10-26 00:09:17 -0500

    Empezando denuevo con C#

---
+++ clase01/AVANCE_NET01/ConsoleApp1/BEAN/ClienteBEAN.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BEAN
+{
+    public class ClienteBEAN
+    {
+        //propfull ambiguo
+        private char genero;
+
+        public char Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+
+        //prop nuevo
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Carrera { get; set; }
+
+        //explicara la diferencia entre prop y propfull
+    }
+}
---
+++ clase01/AVANCE_NET01/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1.BEAN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteBEAN cliBean = new ClienteBEAN();
+            cliBean.Nombre = "Luis";
+            cliBean.Apellido = "Caccha";
+            cliBean.Edad = 22;
+            cliBean.Genero = 'M';
+            cliBean.Carrera = "Ing. Sistemas";
+
+            Console.WriteLine(cliBean.Nombre + " " + cliBean.Apellido);
+            Console.ReadLine();
+
+        }
+    }
+}
