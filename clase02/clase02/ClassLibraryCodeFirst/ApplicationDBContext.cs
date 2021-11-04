2093fb11284f906b77101d41b2bd5bcd45af3cc8
DjangoBv <renato.caccha.7@gmail.com>
2021-11-03 23:09:00 -0500

    clase02 terminada

---
+++ clase02/clase02/ClassLibraryCodeFirst/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCodeFirst
+{
+    public class ApplicationDBContext : DbContext
+    {
+        //Genero el constructor del ApplicationDBContext -> (), y lo uno con mi cadena de conexion
+        public ApplicationDBContext() : base("connBD")
+        {
+
+        }
+        //Persona es un nombre indistinto que no se verá reflejado
+        public DbSet<Persona> Persona { get; set; }
+        public DbSet<TipoDocumento> TipoDocumento { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ clase02/clase02/ClassLibraryCodeFirst/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCodeFirst
+{
+    [Table("tbPersona")]
+    public class Persona
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        //nivel de validaciones
+        [Required]
+        [StringLength(50)]
+        //nivel de esquema
+        [Column("NombrePersona", TypeName ="varchar")]
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad { get; set; }
+        public int IdTipoDoc { get; set; }
+
+        public TipoDocumento TipoDocumento { get; set; }
+    }
+}
